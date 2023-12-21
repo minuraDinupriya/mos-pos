8ea858294ac3efa5a9a23b82a743881c244fcd7f
Minura Dinupriya <mranaweera793@gmail.com>
2023-12-21 19:19:25 +0530

    trying real js array cards

---
+++ asset/js/app.js
@@ -0,0 +1,6 @@
+document.getElementById("test").onclick=function(){
+    let foodName=document.getElementById("big-burger").innerHTML;
+    document.getElementById("food-name").innerHTML=foodName;
+}
+
+
