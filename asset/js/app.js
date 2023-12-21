de3cb971eb145e8814a669697e2ca78b4abac9a0
Minura Dinupriya <mranaweera793@gmail.com>
2023-12-21 19:39:06 +0530

    image added trying to change some bootstrap classes :(

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,6 +1,480 @@
-document.getElementById("test").onclick=function(){
-    let foodName=document.getElementById("big-burger").innerHTML;
-    document.getElementById("food-name").innerHTML=foodName;
-}
-
+let burgerArray = [
+    {
+      itemCode: "B1001",
+      itemName: "Classic Burger (Large)",
+      price: 750.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1002",
+      itemName: "Classic Burger (Regular)",
+      price: 1500.0,
+      discount: "15%",
+    },
+    {
+      itemCode: "B1003",
+      itemName: "Turkey Burger",
+      price: 1600.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1004",
+      itemName: "Chicken Burger (Large)",
+      price: 1400.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1005",
+      itemName: "Chicken Burger (Regular)",
+      price: 800.0,
+      discount: "20%",
+    },
+    {
+      itemCode: "B1006",
+      itemName: "Cheese Burger (Large)",
+      price: 1000.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1007",
+      itemName: "Cheese Burger (Regular)",
+      price: 600.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1008",
+      itemName: "Bacon Burger",
+      price: 650.0,
+      discount: "15%",
+    },
+    {
+      itemCode: "B1009",
+      itemName: "Shawarma Burger",
+      price: 800.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1010",
+      itemName: "Olive Burger",
+      price: 1800.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1012",
+      itemName: "Double-Cheese Burger",
+      price: 1250.0,
+      discount: "20%",
+    },
+    {
+      itemCode: "B1013",
+      itemName: "Crispy Chicken Burger (Regular)",
+      price: 1200.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1014",
+      itemName: "Crispy Chicken Burger (Large)",
+      price: 1600.0,
+      discount: "10%",
+    },
+    {
+      itemCode: "B1015",
+      itemName: "Paneer Burger",
+      price: 900.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1016",
+      itemName: "Crispy Chicken Submarine (Large)",
+      price: 2000.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1017",
+      itemName: "Crispy Chicken Submarine (Regular)",
+      price: 1500.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1018",
+      itemName: "Chicken Submarine (Large)",
+      price: 1800.0,
+      discount: "3%",
+    },
+    {
+      itemCode: "B1019",
+      itemName: "Chicken Submarine (Regular)",
+      price: 1400.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1020",
+      itemName: "Grinder Submarine",
+      price: 2300.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1021",
+      itemName: "Cheese Submarine",
+      price: 2200.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1022",
+      itemName: "Double Cheese n Chicken Submarine",
+      price: 1900.0,
+      discount: "16%",
+    },
+    {
+      itemCode: "B1023",
+      itemName: "Special Horgie Submarine",
+      price: 2800.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1024",
+      itemName: "MOS Special Submarine",
+      price: 3000.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1025",
+      itemName: "Steak Fries (Large)",
+      price: 1200.0,
+    },
+    {
+      itemCode: "B1026",
+      itemName: "Steak Fries (Medium)",
+      price: 600.0,
+    },
+    {
+      itemCode: "B1027",
+      itemName: "French Fries (Large)",
+      price: 800.0,
+    },
+    {
+      itemCode: "B1028",
+      itemName: "French Fries (Medium)",
+      price: 650.0,
+    },
+    {
+      itemCode: "B1029",
+      itemName: "French Fries (Small)",
+      price: 450.0,
+    },
+    {
+      itemCode: "B1030",
+      itemName: "Sweet Potato Fries (Large)",
+      price: 600.0,
+    },
+  
+    {
+      itemCode: "B1031",
+      itemName: "Chicken n Cheese Pasta",
+      price: 1600.0,
+      discount: "15%",
+    },
+    {
+      itemCode: "B1032",
+      itemName: "Chicken Penne Pasta",
+      price: 1700.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1033",
+      itemName: "Ground Turkey Pasta Bake",
+      price: 2900.0,
+      discount: "10%",
+    },
+    {
+      itemCode: "B1034",
+      itemName: "Creamy Shrimp Pasta",
+      price: 2000.0,
+      discount: null,
+    },
+  
+    {
+      itemCode: "B1035",
+      itemName: "Lemon Butter Pasta",
+      price: 1950.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1036",
+      itemName: "Tagliatelle Pasta",
+      price: 2400.0,
+      discount: "1%",
+    },
+    {
+      itemCode: "B1037",
+      itemName: "Baked Ravioli",
+      price: 2000.0,
+      discount: "1%",
+    },
+  
+    {
+      itemCode: "B1038",
+      itemName: "Fried Chicken (Small)",
+      price: 1200.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1039",
+      itemName: "Fried Chicken (Regular)",
+      price: 2300.0,
+      discount: "10%",
+    },
+    {
+      itemCode: "B1040",
+      itemName: "Fried Chicken (Large)",
+      price: 3100.0,
+      discount: "5%",
+    },
+    {
+      itemCode: "B1041",
+      itemName: "Hot Wings (Large)",
+      price: 2400.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1042",
+      itemName: "Devilled Chicken (Large)",
+      price: 900.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1043",
+      itemName: "BBQ Chicken (Regular)",
+      price: 2100.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1044",
+      itemName: "Pepsi (330ml)",
+      price: 990.0,
+      discount: "5%",
+    },
+    {
+      itemCode: "B1045",
+      itemName: "Coca-Cola (330ml)",
+      price: 1230.0,
+      discount: null,
+    },
+    {
+      itemCode: "B1046",
+      itemName: "Sprite (330ml)",
+      price: 1500.0,
+      discount: "3%",
+    },
+    {
+      itemCode: "B1047",
+      itemName: "Mirinda (330ml)",
+      price: 850.0,
+      discount: "7%",
+    },
+  ];
+  
+  // ---------------------------------------------
+  
+  // // A function that takes an array of objects and generates Bootstrap cards
+  // function generateHTML(array) {
+  //   // Create a div element to contain the cards
+  //   let container = document.createElement("div");
+  //   // Use the .container class to add some padding and margin to the container
+  //   container.classList.add("container");
+  //   // Use the .row class to create a row of cards
+  //   let row = document.createElement("div");
+  //   row.classList.add("row");
+  //   // Loop through the array
+  //   array.forEach((item) => {
+  //     // Use the .col class to specify the width of each card
+  //     let col = document.createElement("div");
+  //     col.classList.add("col-md-4");
+  //     // Use the .card class to create a card element
+  //     let card = document.createElement("div");
+  //     card.classList.add("card");
+  //     // Use the .card-body class to add a padded section within the card
+  //     let cardBody = document.createElement("div");
+  //     cardBody.classList.add("card-body");
+  //     // Use the .card-title class to add a title to the card
+  //     let cardTitle = document.createElement("h5");
+  //     cardTitle.classList.add("card-title");
+  //     // Create a text node with the item name value
+  //     let cardTitleText = document.createTextNode(item.itemName);
+  //     // Append the text node to the card title element
+  //     cardTitle.appendChild(cardTitleText);
+  //     // Use the .card-text class to add some text to the card
+  //     let cardText = document.createElement("p");
+  //     cardText.classList.add("card-text");
+  //     // Create a text node with the item code, price, and discount values
+  //     let cardTextText = document.createTextNode(
+  //       `Item Code: ${item.itemCode}\nPrice: ${item.price}\nDiscount: ${
+  //         item.discount ? item.discount : "None"
+  //       }`
+  //     );
+  //     // Append the text node to the card text element
+  //     cardText.appendChild(cardTextText);
+  //     // Append the card title and card text elements to the card body element
+  //     cardBody.appendChild(cardTitle);
+  //     cardBody.appendChild(cardText);
+  //     // Append the card body element to the card element
+  //     card.appendChild(cardBody);
+  //     // Append the card element to the col element
+  //     col.appendChild(card);
+  //     // Append the col element to the row element
+  //     row.appendChild(col);
+  //   });
+  //   // Append the row element to the container element
+  //   container.appendChild(row);
+  //   // Return the container element
+  //   return container;
+  // }
+  
+  // A function that takes an array of objects and generates Bootstrap cards
+  function generateHTML(array) {
+    // Create a div element to contain the cards
+    let container = document.createElement("div");
+    // Use the .container class to add some padding and margin to the container
+    container.classList.add("container");
+    // Use the .row class to create a row of cards
+    let row = document.createElement("div");
+    row.classList.add("row");
+  //   row.classList.add("d-flex","flex-wrap");
+    // Loop through the array
+    array.forEach((item) => {
+      // Use the .col class to specify the width of each card
+      let col = document.createElement("div");
+      col.classList.add("col-lg-3");
+      // Use the .card class to create a card element
+      let card = document.createElement("div");
+      card.classList.add("card");
+      // Use the .card-body class to add a padded section within the card
+      let cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+      // Use the .card-title class to add a title to the card
+      let cardTitle = document.createElement("h6");
+      cardTitle.classList.add("card-title");
+      // Create a text node with the name value
+      let cardTitleText = document.createTextNode(item.itemName);
+      // Append the text node to the card title element
+      cardTitle.appendChild(cardTitleText);
+      // Use the .card-text class to add some text to the card
+      let cardText = document.createElement("p");
+      cardText.classList.add("card-text");
+      // Create a text node with the description value
+      let cardTextText = document.createTextNode(item.itemCode);
+      // Append the text node to the card text element
+      cardText.appendChild(cardTextText);
+  
+      let image=document.createElement("img");
+      image.src="./asset/images/pic.jpg";
+      image.classList.add("card-img-top");
+      cardBody.appendChild(image);
+  
+      // Append the card title and card text elements to the card body element
+      cardBody.appendChild(cardTitle);
+      cardBody.appendChild(cardText);
+  
+      let priceTag=document.createElement("h5");
+      let price=document.createTextNode(item.price);
+      priceTag.appendChild(price);
+      cardBody.appendChild(priceTag);
+  
+      let discountTag=document.createElement("h6");
+      let discount=document.createTextNode(item.discount);
+      discountTag.appendChild(discount);
+      cardBody.appendChild(discountTag);
+  
+      let button=document.createElement("button");
+      button.textContent="Add to Cart";
+      button.classList.add("btn", "btn-danger");
+      cardBody.appendChild(button);
+  
+      // Append the card body element to the card element
+      card.appendChild(cardBody);
+      // Append the card element to the col element
+      col.appendChild(card);
+      // Append the col element to the row element
+      row.appendChild(col);
+    });
+    // Append the row element to the container element
+    container.appendChild(row);
+    // Return the container element
+    return container;
+  }
+  
+  // Call the function and pass the food array as an argument
+  let result = generateHTML(burgerArray);
+  // Append the result to the document body
+  document.body.appendChild(result);
+  
+  // this works
+  
+  // // Call the function and pass the burger array as an argument
+  // let result = createHTML(burgerArray);
+  // // Append the result to the document body
+  // document.body.appendChild(result);
+  
+  // // ---------------------------------------------
+  
+  // // A function that takes an array of objects and generates Bootstrap cards with buttons
+  // function createHTML(array) {
+  //     // Create a div element to contain the cards
+  //     let container = document.createElement("div");
+  //     // Use the .container class to add some padding and margin to the container
+  //     container.classList.add("container");
+  //     // Use the .row class to create a row of cards
+  //     let row = document.createElement("div");
+  //     row.classList.add("row");
+  //     // Loop through the array
+  //     array.forEach((item) => {
+  //       // Use the .col class to specify the width of each card
+  //       let col = document.createElement("div");
+  //       col.classList.add("col-md-4");
+  //       // Use the .card class to create a card element
+  //       let card = document.createElement("div");
+  //       card.classList.add("card");
+  //       // Use the .card-body class to add a padded section within the card
+  //       let cardBody = document.createElement("div");
+  //       cardBody.classList.add("card-body");
+  //       // Use the .card-title class to add a title to the card
+  //       let cardTitle = document.createElement("h5");
+  //       cardTitle.classList.add("card-title");
+  //       // Create a text node with the item name value
+  //       let cardTitleText = document.createTextNode(item.itemName);
+  //       // Append the text node to the card title element
+  //       cardTitle.appendChild(cardTitleText);
+  //       // Use the .card-text class to add some text to the card
+  //       let cardText = document.createElement("p");
+  //       cardText.classList.add("card-text");
+  //       // Create a text node with the item code, price, and discount values
+  //       let cardTextText = document.createTextNode(
+  //         `Item Code: ${item.itemCode}\nPrice: ${item.price}\nDiscount: ${
+  //           item.discount ? item.discount : "None"
+  //         }`
+  //       );
+  //       // Append the text node to the card text element
+  //       cardText.appendChild(cardTextText);
+  //       // Create a button element with the text Add to Cart
+  //       let button = document.createElement("button");
+  //       button.textContent = "Add to Cart";
+  //       // Use the .btn and .btn-primary classes to style the button
+  //       button.classList.add("btn", "btn-primary");
+  //       // Append the card title, card text, and button elements to the card body element
+  //       cardBody.appendChild(cardTitle);
+  //       cardBody.appendChild(cardText);
+  //       cardBody.appendChild(button);
+  //       // Append the card body element to the card element
+  //       card.appendChild(cardBody);
+  //       // Append the card element to the col element
+  //       col.appendChild(card);
+  //       // Append the col element to the row element
+  //       row.appendChild(col);
+  //     });
+  //     // Append the row element to the container element
+  //     container.appendChild(row);
+  //     // Return the container element
+  //     return container;
+  //   }
+  
 
